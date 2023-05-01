1c528fed40704a61803c404b74cbfa7a748ee851
Greg Larrenaga <greglarrenaga@gmail.com>
2023-05-01 06:05:03 +0000

    Prefer jsdom environment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
 	},
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom',
 	}
 });
