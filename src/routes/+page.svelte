b4df7c748647b59422c604a340b7d3423e90e225
Greg Larrenaga <greglarrenaga@gmail.com>
2023-05-02 01:54:54 +0000

    Adds tailwind support

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,3 +29,11 @@
 		<div tabindex="1" />
 	</fieldset>
 </div>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
